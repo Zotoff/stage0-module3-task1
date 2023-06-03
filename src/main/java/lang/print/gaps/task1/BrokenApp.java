dc9c990920d2f9272013eeaf58bdcdbd19269dfa
Zotoff <paulzotoff@gmail.com>
2023-06-03 13:15:22 +0300

    Third attempt, remove err

--- src/main/java/lang/print/gaps/task1/BrokenApp.java
+++ src/main/java/lang/print/gaps/task1/BrokenApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class BrokenApp {
     public static void main(String[] args) {
-        System.err.println("fixMe");
+        System.out.println("fixMe");
     }
 }
--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -3,8 +3,8 @@ package lang.print.gaps.task1;
 public class PrintWordsApp {
     public static void main(String[] args) {
         //write your code here
-        System.err.println("java");
-        System.err.println("is");
-        System.err.println("smth");
+        System.out.println("java");
+        System.out.println("is");
+        System.out.println("smth");
     }
 }
